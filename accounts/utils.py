90df50e958da90793278f884767cdb6c99e6b4b4
DenkoProg <kowaldenysproekt@gmail.com>
2023-08-18 10:43:08 +0300

    email activation template

--- accounts/utils.py
+++ accounts/utils.py
@@ -29,6 +29,7 @@ def send_verification_email(request, user):
     })
     to_email = user.email
     mail = EmailMessage(mail_subject, message, to=[to_email])
+    mail.content_subtype = "html"
     mail.send()
 
 
@@ -44,6 +45,7 @@ def send_password_reset_email(request, user):
     })
     to_email = user.email
     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.content_subtype = "html"
     mail.send()
 
 
@@ -56,4 +58,5 @@ def send_notification(mail_subject, mail_template, context):
     else:
         to_email = context['to_email']
     mail = EmailMessage(mail_subject, message, from_email, to=to_email)
+    mail.content_subtype = "html"
     mail.send()
