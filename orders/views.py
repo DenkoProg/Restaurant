c25d92d29facab6ec65a8c7f98485ff8d07a4378
DenkoProg <kowaldenysproekt@gmail.com>
2023-08-18 10:26:20 +0300

    orders and order detail functionalities - customer/vendor

--- orders/views.py
+++ orders/views.py
@@ -8,6 +8,8 @@ from .utils import generate_order_number
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
+from menu.models import FoodItem
+from marketplace.models import Tax
 
 
 @login_required(login_url='login')
@@ -16,6 +18,34 @@ def place_order(request):
     cart_count = cart_items.count()
     if cart_count <= 0:
         redirect('marketplace')
+
+    vendors_ids = []
+    for i in cart_items:
+        if i.fooditem.vendor.id not in vendors_ids:
+            vendors_ids.append(i.fooditem.vendor.id)
+
+    get_tax = Tax.objects.filter(is_active=True)
+    total_data = {}
+    k = {}
+    for i in cart_items:
+        fooditem = FoodItem.objects.get(pk=i.fooditem.id, vendor_id__in=vendors_ids)
+        v_id = fooditem.vendor._check_id_field()
+        if v_id in k:
+            subtotal = k[v_id]
+            subtotal += (fooditem.price * i.quantity)
+            k[v_id] = subtotal
+        else:
+            subtotal = (fooditem.price * i.quantity)
+            k[v_id] = subtotal
+
+        tax_dict = {}
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage * subtotal) / 100, 2)
+            tax_dict.update({tax_type: {str(tax_percentage): str(tax_amount)}})
+
+        total_data.update({fooditem.vendor.id: {str(subtotal): str(tax_dict)}})
 
     subtotal = get_cart_amounts(request)['subtotal']
     total_tax = get_cart_amounts(request)['tax']
@@ -38,10 +68,12 @@ def place_order(request):
             order.user = request.user
             order.total = grand_total
             order.tax_data = json.dumps(tax_data)
+            order.total_data = json.dumps(total_data)
             order.total_tax = total_tax
             order.payment_method = 'PayPal'
             order.save()
             order.order_number = generate_order_number(order.id)
+            order.vendors.add(*vendors_ids)
             order.save()
             context = {
                 'order': order,
--- restaurant_main/settings.py
+++ restaurant_main/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'orders.request_object.RequestObjectMiddleware', # Custom middleware
 ]
 
 ROOT_URLCONF = 'restaurant_main.urls'
